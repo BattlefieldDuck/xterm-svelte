6c3cfffcfa72ce1523296a3b3ca71be9f94645d1
TatLead <hkdsethomas@gmail.com>
2024-11-04 15:50:45 +0800

    Remove @xterm/addon-ligatures

--- src/lib/XtermAddonType.ts
+++ src/lib/XtermAddonType.ts
@@ -3,7 +3,7 @@ export type * from '@xterm/addon-canvas';
 export type * from '@xterm/addon-clipboard';
 export type * from '@xterm/addon-fit';
 export type * from '@xterm/addon-image';
-export type * from '@xterm/addon-ligatures';
+// export type * from '@xterm/addon-ligatures';
 export type * from '@xterm/addon-search';
 export type * from '@xterm/addon-serialize';
 // export type * from '@xterm/addon-unicode-graphemes';
