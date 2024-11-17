cf320339dee0fbdf5556fada4c273ccd82382193
TatLead <hkdsethomas@gmail.com>
2024-11-18 03:20:12 +0800

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to xterm-svelte' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Welcome to Xterm Svelte' })).toBeVisible();
 });
 
 test('Xterm component is visible', async ({ page }) => {
